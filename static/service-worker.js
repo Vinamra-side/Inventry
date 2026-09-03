dfa47e1c8925e37b32552a1846dcb623dcbccc85
VinamraOberoi <oberoivinamra@gmail.com>
2026-09-04 03:24:57 +0530

    Optimize stock card grid layout

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "saiko-static-v12";
+const CACHE_NAME = "saiko-static-v13";
 const STATIC_ASSETS = [
-  "/static/style.css?v=12",
+  "/static/style.css?v=13",
   "/static/pwa.js?v=4",
   "/static/saiko-logo-clean.png",
   "/static/saiko-logo.png",

894ff46f54933265e529480f59aacb508ea7004e
VinamraOberoi <oberoivinamra@gmail.com>
2026-09-04 02:35:25 +0530

    User menu update

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "saiko-static-v4";
+const CACHE_NAME = "saiko-static-v5";
 const STATIC_ASSETS = [
-  "/static/style.css?v=4",
+  "/static/style.css?v=5",
   "/static/pwa.js?v=4",
   "/static/saiko-logo-clean.png",
   "/static/saiko-logo.png",
